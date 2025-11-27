bd8c4934f35a474a473865ab157b235b736a0ebe
Brian Obot <brianobot@gmail.com>
2025-11-27 17:52:12 +0100

    Add Logic for database.py file and minimal content for dependencies

---
+++ app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+from app.settings import Settings
+
+settings = Settings()
+
+# Add Support for Both ASYNC and SYNC Database URLs
+# With Async being the center of focus
+DATABASE_URL = settings.DATABASE_URL
+DATABASE_SYNC_URL = settings.DATABASE_URL.replace("+asyncpg", "")
+
+sync_engine = create_engine(DATABASE_SYNC_URL)
+SyncSessionLocal = sessionmaker(bind=sync_engine)
+
+async_engine = create_async_engine(DATABASE_URL)
+AsyncSessionLocal = async_sessionmaker(bind=async_engine, expire_on_commit=False)
--- app/dependencies.py
+++ app/dependencies.py
@@ -1,12 +1,13 @@
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession
-# from app.database import SessionLocal
 
+from app.database import AsyncSessionLocal
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
-    # ignore for brevity
-    # async with SessionLocal() as session:
-    #     yield session
-    pass 
+    async with AsyncSessionLocal() as session:
+        yield session
+
+
+
 
