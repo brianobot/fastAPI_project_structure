4d2243b31eca1b61ad536eac29733c5afc6f15a3
Brian Obot <brianobot@gmail.com>
2026-01-05 14:16:48 +0100

    Add Email related configurations fot the setting model to support thier usage in the mailer module

--- app/settings.py
+++ app/settings.py
@@ -12,3 +12,10 @@ class Settings(BaseSettings):
     
     REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6379
+    
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: str
+    MAIL_SERVER: str
+    MAIL_FROM_NAME: str
